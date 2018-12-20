9a883f4ab2f745c12dedbc6008df9f58d0d506d4
Carlton Xavier <carlton.xavier@helsinki.fi>
2018-12-20 09:51:37 +0200

    Documents folder added

---
+++ Definitions.f90
@@ -0,0 +1,10 @@
+!!!! This module contains the definitions for the data used in the new "Superbox-Model"
+!! First version developlment 5.12.2018
+
+module definitions
+
+implicit none
+public
+ 
+
+end module
