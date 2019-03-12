de76727ac46de6b3cac90285d543c462f70a5243
Petri Clusius <petri.clusius@helsinki.fi>
2019-03-12 23:32:32 +0200

    Added 0 centigrade in Kelvin

--- constants.f90
+++ constants.f90
@@ -5,8 +5,10 @@ PUBLIC
 
 real(dp), parameter   :: Na = 6.022140857d23  ! 1/mol Avogadro constant
 real(dp), parameter   :: R  = 8.3144598       ! [J/K/mol] Universal gas constant
+real(dp), parameter   :: R  = 8.3144598       ! [J/K/mol] Universal gas constant
 real(dp), parameter   :: kb = R/Na            ! [J/K] Boltzmann constant
-real(dp), parameter   :: pi = ACOS(-1d0)         ! pi
+real(dp), parameter   :: pi = ACOS(-1d0)      ! pi
+real(dp), parameter   :: K0 = 273.15          ! [K] Zero degree celcius in K
 integer(ik), parameter:: min_s = 60           ! [s] seconds in minute
 integer(ik), parameter:: hour_s  = 3600       ! [s] seconds in hour
 integer(ik), parameter:: day_s = 24*hour_s
