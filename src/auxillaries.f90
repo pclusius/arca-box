6e6258fb681e5c623fc971cfe245312908f0e86f
Petri Clusius <petri.clusius@helsinki.fi>
2019-03-25 16:06:14 +0200

    Fixed a potential bug in C_AIR_m3()

--- src/auxillaries.f90
+++ src/auxillaries.f90
@@ -46,7 +46,7 @@ CONTAINS
     C_AIR_cc = P/(kb*T)*1d-6
   end function C_AIR_cc
   ! concentration of air. Pressure P in [Pa], temperature T in [K] returns molecules/m3
-  real(dp) function C_AIR_m3(P,T)
+  real(dp) function C_AIR_m3(T,P)
     REAL(dp) :: T, P
     C_AIR_m3 = P/(kb*T)
   end function C_AIR_m3
@@ -65,6 +65,7 @@ CONTAINS
 ! t = time in seconds (real8)
 ! check = minute that is to be checked (integer)
 logical function EVENMIN(t,check)
+  IMPLICIT NONE
   INTEGER :: check
   REAL(dp):: t
   IF (MODULO(int(t+0.5d0), 60*check) == 0) THEN
@@ -83,6 +84,7 @@ end function EVENMIN
 ! If minimum >= maximum, minumum value is used as constant concentration
 !..............................................................................
 REAL(dp) FUNCTION NORMALD(time,MODS)
+  IMPLICIT NONE
   type(parametered_input) :: MODS
   REAL(dp) :: time
   REAL(dp) :: f, D
@@ -102,5 +104,18 @@ REAL(dp) FUNCTION NORMALD(time,MODS)
     END IF
   END IF
 end FUNCTION NORMALD
+
+!==============================================================================
+! Function to return y-value at [time] from a sine function. Use to model a
+! periodic variable such as temperature or relative humidity. By default uses
+! 24 hour period.
+!..............................................................................
+REAL(dp) FUNCTION SIND(time,MODS)
+  IMPLICIT NONE
+  type(parametered_input) :: MODS
+  REAL(dp) :: time, A
+  A = (MODS%max_c-MODS%min_c)/2d0
+  SIND = A*SIN(2d0*pi*time/3600d0/24d0 - 3d0*pi/4d0) + MODS%min_c + A
+end FUNCTION SIND
 
 end MODULE AUXILLARIES
