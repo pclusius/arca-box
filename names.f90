d7a51477d71413c5103faa6fa5f914433f360198
Petri Clusius <petri.clusius@helsinki.fi>
2019-04-12 13:28:43 +0300

    Variable names

---
+++ names.f90
@@ -0,0 +1,17 @@
+program NAME_MOD
+IMPLICIT NONE
+CHARACTER(len=25), parameter :: NAMES(6) = (/   &
+'Temperature',&
+'H2SO4      ',&
+'DMA        ',&
+'CS         ',&
+'NH3        ',&
+! 'O3         ',&
+! 'NO         ',&
+! 'NO2        ',&
+! 'NO3        ',&
+'Pressure   '&
+/)
+
+print*, NAMES
+END program NAME_MOD
