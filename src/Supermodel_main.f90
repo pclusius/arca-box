5024a367b0e2cf580bcbf7e6399e1652d940be37
Petri Clusius <petri.clusius@helsinki.fi>
2019-04-23 15:42:34 +0300

    Changed names of ind_ variables

--- src/Supermodel_main.f90
+++ src/Supermodel_main.f90
@@ -12,7 +12,7 @@ use OUTPUT
 
 IMPLICIT NONE
 ! MOST OF THE VARIABLES ARE DEFINED IN INPUT.F90
-REAL(dp) :: TSTEP_CONC(ind_LAST) = 0d0
+REAL(dp) :: TSTEP_CONC(inm_LAST) = 0d0
 REAL(dp) :: J_ACDC_NH3
 REAL(dp) :: J_ACDC_DMA
 REAL(dp) :: J_NH3_BY_IONS(3)
@@ -35,7 +35,7 @@ INTEGER  :: I
 
 ! =================================================================================================
 ! Assign values to input variables
-  DO I = 1, IND_LAST ! <-- IND_last will cycle through all variables that user can provide or tamper, and leave zero if no input or mod was provided
+  DO I = 1, inm_LAST ! <-- inm_last will cycle through all variables that user can provide or tamper, and leave zero if no input or mod was provided
     IF ((INDRELAY(I,2)>0) .or. (MODS(I)%MODE > 0) .or. (ABS(MODS(I)%Shift-0d0) > 1d-100)) THEN
       TSTEP_CONC(I) = interp(timevec, CONC_MAT(:,I))  .mod. MODS(I)
       ! INDRELAY(I,2)>0 means that user must have provided a column from an input file; MODS(I)%MODE > 0 means NORMALD is in use
@@ -59,8 +59,8 @@ INTEGER  :: I
 ! =================================================================================================
 ! Write printouts to screen and outputs to netcdf-file, later this will include more optionality
   if (MODELTIME%printnow) CALL PRINT_KEY_INFORMATION(TSTEP_CONC)
-  if (MODELTIME%savenow) CALL SAVE_GASES(TSTEP_CONC(ind_temp), TSTEP_CONC(ind_H2SO4), TSTEP_CONC(ind_nh3),&
-                              TSTEP_CONC(ind_dma),J_ACDC_NH3, J_ACDC_DMA, TSTEP_CONC(ind_cs), MODS)
+  if (MODELTIME%savenow) CALL SAVE_GASES(TSTEP_CONC(inm_temp), TSTEP_CONC(inm_H2SO4), TSTEP_CONC(inm_nh3),&
+                              TSTEP_CONC(inm_dma),J_ACDC_NH3, J_ACDC_DMA, TSTEP_CONC(inm_cs), MODS)
 ! =================================================================================================
 
 ! =================================================================================================
@@ -117,13 +117,13 @@ real(dp), intent(in) :: C(:)
 real(dp)             :: H2SO4,NH3,DMA,IPR ! these are crated to make the unit conversion
 
 ! NUCLEATION BY S-ACID AND NH3 - NOTE: ingoing concentrations are assumed to be in 1/m3!!
-  H2SO4 = C(IND_H2SO4)*1d6
-  NH3   = C(IND_NH3)*1d6
-  DMA   = C(IND_DMA)*1d6
-  IPR   = C(IND_IPR)*1d6
+  H2SO4 = C(inm_H2SO4)*1d6
+  NH3   = C(inm_NH3)*1d6
+  DMA   = C(inm_DMA)*1d6
+  IPR   = C(inm_IPR)*1d6
   ! Speed up program by ignoring nucleation when there is none
   if (NH3 > 1d12 .or. J_ACDC_NH3 > 1d-6) THEN
-    CALL get_acdc_J(H2SO4,NH3,c_org,C(IND_CS),C(IND_TEMP),IPR,MODELTIME,&
+    CALL get_acdc_J(H2SO4,NH3,c_org,C(inm_CS),C(inm_TEMP),IPR,MODELTIME,&
           ACDC_solve_ss,J_ACDC_NH3,acdc_cluster_diam, J_NH3_BY_IONS)
   ELSE
     ! This will leave the last value for J stand - small enough to not count but not zero
@@ -132,7 +132,7 @@ real(dp)             :: H2SO4,NH3,DMA,IPR ! these are crated to make the unit co
 
   ! Speed up program by ignoring nucleation when there is none
   if (DMA > 1d6 .or. J_ACDC_DMA > 1d-6) THEN
-    CALL get_acdc_D(H2SO4,DMA,c_org,C(IND_CS),C(IND_TEMP),MODELTIME,ACDC_solve_ss,J_ACDC_DMA,acdc_cluster_diam)
+    CALL get_acdc_D(H2SO4,DMA,c_org,C(inm_CS),C(inm_TEMP),MODELTIME,ACDC_solve_ss,J_ACDC_DMA,acdc_cluster_diam)
   ELSE
     ! This will leave the last value for J stand - small enough to not count but not zero
     if (MODELTIME%printnow) print FMT_SUB, 'DMA IGNORED'
@@ -153,11 +153,11 @@ END SUBROUTINE SOME_OTHER_NUCLEATION_TYPE
 SUBROUTINE PRINT_KEY_INFORMATION(C)
   IMPLICIT NONE
   real(dp), intent(in) :: C(:)
-  print FMT10_2CVU,'ACID C: ', C(IND_H2SO4), ' [1/cm3]', 'Temp:', C(IND_TEMP), 'Kelvin'
-  print FMT10_2CVU, 'NH3 C:', C(IND_NH3), ' [1/cm3]','J_NH3:', J_ACDC_NH3*1d-6, ' [1/cm3]'
-  print FMT10_2CVU, 'DMA C:', C(IND_DMA) , ' [1/cm3]','J_DMA:', J_ACDC_DMA*1d-6, ' [1/cm3]'
+  print FMT10_2CVU,'ACID C: ', C(inm_H2SO4), ' [1/cm3]', 'Temp:', C(inm_TEMP), 'Kelvin'
+  print FMT10_2CVU, 'NH3 C:', C(inm_NH3), ' [1/cm3]','J_NH3:', J_ACDC_NH3*1d-6, ' [1/cm3]'
+  print FMT10_2CVU, 'DMA C:', C(inm_DMA) , ' [1/cm3]','J_DMA:', J_ACDC_DMA*1d-6, ' [1/cm3]'
   print FMT10_3CVU, 'Jion1:', J_NH3_BY_IONS(1)*1d-6 , ' [1/s/cm3]','Jion2:', J_NH3_BY_IONS(2)*1d-6 , ' [1/s/cm3]','Jion2:', J_NH3_BY_IONS(3)*1d-6 , ' [1/s/cm3]'
-  print FMT10_2CVU, 'C-sink:', C(IND_CS) , ' [1/s]','IPR:', C(IND_IPR) , ' [1/s/cm3]'
+  print FMT10_2CVU, 'C-sink:', C(inm_CS) , ' [1/s]','IPR:', C(inm_IPR) , ' [1/s/cm3]'
   print FMT_LEND,
 END SUBROUTINE PRINT_KEY_INFORMATION
 
@@ -196,8 +196,8 @@ SUBROUTINE PRINT_FINAL_VALUES_IF_LAST_STEP_DID_NOT_DO_IT_ALREADY
     print FMT_TIME, MODELTIME%hms
     CALL PRINT_KEY_INFORMATION(TSTEP_CONC)
   END IF
-  if (.not. MODELTIME%savenow) call SAVE_GASES(TSTEP_CONC(ind_temp), TSTEP_CONC(ind_H2SO4), &
-  TSTEP_CONC(ind_nh3), TSTEP_CONC(ind_dma),J_ACDC_NH3, J_ACDC_DMA, TSTEP_CONC(ind_cs), MODS)
+  if (.not. MODELTIME%savenow) call SAVE_GASES(TSTEP_CONC(inm_temp), TSTEP_CONC(inm_H2SO4), &
+  TSTEP_CONC(inm_nh3), TSTEP_CONC(inm_dma),J_ACDC_NH3, J_ACDC_DMA, TSTEP_CONC(inm_cs), MODS)
 END SUBROUTINE PRINT_FINAL_VALUES_IF_LAST_STEP_DID_NOT_DO_IT_ALREADY
 
 
