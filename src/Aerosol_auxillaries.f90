795a1e28602bd7c732fe4bfbc3b1feefdad18102
Carlton Xavier <carlton.xavier@helsinki.fi>
2019-05-06 13:30:24 +0300

    Aerosol_aux added

---
+++ src/Aerosol_auxillaries.f90
@@ -0,0 +1,25 @@
+Module Aerosol_auxillaries
+
+Use constants
+Use SECOND_PRECISION, only:dp
+Use AUXILLARIES
+
+implicit NONE
+PUBLIC
+
+real(dp),dimension(3)::N_speed !! nucleation, condesnation,coagulation
+real(dp) :: precision
+logical :: first_time
+
+contains
+
+subroutine set_speed()
+
+if (First_time) THEN
+  N_speed(:) = 1.0
+  First_time = .False.
+end if
+
+end subroutine set_speed
+
+End module Aerosol_auxillaries
