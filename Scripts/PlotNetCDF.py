55143554e0415bc065967389c43455713e384658
pecl <petri.clusius@helsinki.fi>
2019-11-25 18:57:36 +0200

    updates for command line call

--- Scripts/PlotNetCDF.py
+++ Scripts/PlotNetCDF.py
@@ -5,6 +5,13 @@ from matplotlib.widgets import RadioButtons, Button
 # import sys; import os; sys.path.append(os.path.abspath("/home/pecl/p_modules"))
 # import custom_lib as c
 
+class compound:
+    def __init__(self, var):
+        self.name = var
+    def pl(self):
+        plot_var(self.name)
+        return
+
 
 def roundradios(radioax, bt, scale):
     rpos = radioax.get_position().get_points()
@@ -24,27 +31,14 @@ def uniq(list):
 pth = '/media/pecl/EE7B-B587/PC180416/N1/'
 pth = ''
 pth = '../output/'
-files=(['001NJ_D100.nc','001NJ_K-35_D100.nc','001NJ_K-70_D100.nc'])
-files=(['001NJ_nominal.nc','001NJ_K-35.nc','001NJ_K-70.nc','001NJ_CS05.nc','001NJ_K-35_CS05.nc','001NJ_K-70_CS05.nc'])
-files=([ 'NANJING_check.nc'])
-files=([ 'particle.nc'])
-files=([ 'Hyde_tempSensi.nc'])
-files = (['Hyde_180407_low_nh3.nc', 'Hyde_180407.nc'])
-files = (['Hyde_180412_low_nh3.nc', 'Hyde_180412.nc'])
-files = (['Hyde_180423_low_nh3.nc', 'Hyde_180423.nc'])
-files = (['Hyde_180403_low_nh3.nc'])
-files = (['clustering_HighCS_SA_e5.nc','clustering_HighCS_SA_e6.nc','clustering_HighCS_SA_e7.nc','clustering_HighCS_SA_e8.nc'])
-files = (['clustering_SA_e4.nc','clustering_SA_e5.nc','clustering_SA_e6.nc','clustering_SA_e7.nc','clustering_SA_e8.nc'])
-files = (['clustering_SA_sigma.nc','clustering_SA_mean.nc'])
-files = (['Hyde_180404_low_nh3.nc', 'Hyde_180404.nc'])
-files = (['Hyde_180410.nc', 'Hyde_180410_no_SS.nc'])
+files = (['TESTING_simple_run_general.nc'])
 
-files = (['Hyde_180410_SS.nc', 'Hyde_180410_FF.nc'])
-files = (['/home/pecl/malte/dMalte/Malte_out/Box/PC170408/XM/particle.nc','/home/pecl/malte/dMalte/Malte_out/Box/PC180410/N1/particle.nc'])
-files = (['clustering_TEMP_sensi.nc', 'clustering_TEMP_sensi_LotsaNH3.nc'])
-files = (['clustering_TEMP_sensi.nc', 'clustering_TEMP_sensi_LotsaNH3.nc', 'clustering_TEMP_sensi_evenmoreNH3.nc'])
-files = (['clustering_TEMP_sensi_evenmoreNH3SA.nc', 'clustering_TEMP_sensi_evenmoreNH3.nc'])
-files = (['TESTING_simple_run.nc'])
+import sys
+cmdline = False
+if len(sys.argv) > 1:
+    files = [str(sys.argv[1])]
+    pth=''
+    cmdline = True
 
 
 N_f = len(files)
@@ -71,10 +65,20 @@ for i,v in enumerate(cache):
     if (len(np.shape(ncs[0].variables[v][:])) >1):
         hnames.append(v)
     # elif (len(uniq(ncs[0].variables[v][:]))>1):
+    if not 'Shifter' in v and not 'Multipl' in v:
     hnames.append(v)
 
+for var in hnames:
+    strg = var+"=compound('%s')"%(var)
+    exec(strg)
+
 ax.set_title(file)
+
+if len(hnames)<60:
 plt.subplots_adjust(left=0.35, bottom=0.1)
+else:
+    plt.subplots_adjust(left=0.1, bottom=0.1)
+
 for jj in range(N_f):
     try:
         plt.plot(ncs[0].variables[ncs[0].variables[hnames[0]].dimensions[0]],
@@ -84,13 +88,18 @@ for jj in range(N_f):
 
 plt.legend(loc='upper right')
 
-rax = plt.axes([0.35, 0.78, 0.1, 0.1], facecolor='w', alpha=0.1)
+if len(hnames)<60:
+    rax = plt.axes([0.36, 0.78, 0.1, 0.1], facecolor='w', alpha=0.1)
+else:
+    rax = plt.axes([0.1, 0.78, 0.1, 0.1], facecolor='w', alpha=0.1)
+
 radio = RadioButtons(rax, ('linear','log'), active=0)
 
+if len(hnames)<60:
 axrads = plt.axes([0.05, 0.1, 0.2, 0.8], facecolor='w')
 radplt = RadioButtons(axrads, (hnames), active=0)
+    roundradios(axrads, radplt,1)
 
-roundradios(axrads, radplt,2)
 roundradios(rax, radio,1)
 
 def scalefunc(scale):
@@ -115,9 +124,21 @@ def update(val):
 radio.on_clicked(scalefunc)
 radio.on_clicked(update)
 
+if len(hnames)<60:
 radplt.on_clicked(plot_var)
 radplt.on_clicked(update)
 
-ax.grid(True)
+def NAMES():
+    for i in range(len(hnames)//6 + 1):
+        print(hnames[i*5+0],'  ',hnames[i*5+1],'  ',hnames[i*5+2],'  ',hnames[i*5+3],'  ',hnames[i*5+4],'  ',hnames[i*5+5])
+    return
+print("use <variables>.pl() to plot any variable. E.g. O3.plot() for Ozone. NAMES() to show all variables")
 
+
+ax.grid(True)
+if (not cmdline):
+    plt.ion()
 plt.show()
+
+if (cmdline):
+    raise SystemExit
