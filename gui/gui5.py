ecd28e2d8eb14108c2a0ee1d92b2c9e567a61c11
pecl <petri.clusius@helsinki.fi>
2020-01-16 17:20:28 +0200

    WIP

--- gui/gui5.py
+++ gui/gui5.py
@@ -1242,7 +1242,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(5)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
