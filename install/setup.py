6872c5f6f6349d254e184a3852205ff681137a66
pecl <petri.clusius@helsinki.fi>
2022-02-25 17:06:30 +0200

    works

--- install/setup.py
+++ install/setup.py
@@ -20,6 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 =============================================================================
 """
 
+
 import sys
 import os
 from shutil import copyfile as cpf
