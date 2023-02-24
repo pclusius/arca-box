81ff7a72a700a37a6d2d4f82b09e0b9b012eb675
pecl <petri.clusius@helsinki.fi>
2023-02-24 14:22:12 +0200

    constant reaction rates handled properly with RATES.dat

--- ModelLib/required/save_reactions.f90
+++ ModelLib/required/save_reactions.f90
@@ -1,8 +1,17 @@
 program save_reactions
   use second_Monitor, only: EQN_NAMES
   integer :: i
-  print*, '       order       index                         reaction'
+  real :: RCONST(size(EQN_NAMES))
+  CHARACTER(len=100) :: TEXT(size(EQN_NAMES)) = ''
+  RCONST = -1.0
+
+
+
+
+
+  print*, '  order   index   constant_R                       reaction'
   do i=1,size(EQN_NAMES)
-	print*,1,i, EQN_NAMES(i)
+  if (RCONST(i)>-1.0) print'(2i8,es12.3,a)',0,i,RCONST(i), EQN_NAMES(i)
+  if (RCONST(i)< 0.0) print'(2i8,a12,a,a)',0,i,'    -   ', EQN_NAMES(i),TEXT(i)
   end do
 end program save_reactions
--- ModelLib/required/write_reactions.sh
+++ ModelLib/required/write_reactions.sh
@@ -1,3 +1,14 @@
+#/user/bin/bash
+head -7 tmp.f90>save_reactions.f90
+grep -E 'RCONST\([0-9]*\)' second_Initialize.f90>>save_reactions.f90
+grep -E 'RCONST\([0-9]*\)' second_Rates.f90|grep -v !>text_rates.txt
+sed -i 's|RCONST|TEXT|g' text_rates.txt
+sed -i 's| = | = "|g' text_rates.txt
+sed -i 's|)$|)"|g' text_rates.txt
+
+cat text_rates.txt>>save_reactions.f90
+tail -7 tmp.f90>>save_reactions.f90
+
 gfortran second_Monitor.f90 save_reactions.f90 -o write_reactions.exe
 ./write_reactions.exe>reactions.txt
-rm second_monitor.mod write_reactions.exe
+rm second_monitor.mod write_reactions.exe tmp.f90 save_reactions.f90
