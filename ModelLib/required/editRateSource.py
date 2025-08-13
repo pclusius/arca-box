59ae096bf1cf8498914d131403eef36fe1fd153a
pclusius <petri.clusius@helsinki.fi>
2025-08-13 15:58:30 +0300

    regex raw strings

--- ModelLib/required/editRateSource.py
+++ ModelLib/required/editRateSource.py
@@ -9,9 +9,9 @@ hits = 0
 ratesfile = ''
 with open(os.path.join(f,'second_Rates.f90'), 'r') as file:
     for line in file:
-        if re.search("RCONST\s*=\s*RCONST\s*\\*\s*R_F",line.strip('\n').upper()):
+        if re.search(r"RCONST\s*=\s*RCONST\s*\*\s*R_F",line.strip('\n').upper()):
             hits +=1
-        if re.search('^\s*END\s*SUBROUTINE\s*UPDATE_RCONST', line.upper()) and hits == 0:
+        if re.search(r'^\s*END\s*SUBROUTINE\s*UPDATE_RCONST', line.upper()) and hits == 0:
             ratesfile += '  RCONST = RCONST * R_F\n'
         ratesfile += line
     if hits>1:
@@ -30,13 +30,13 @@ hits = 0
 mainfile = ''
 with open(os.path.join(f,'second_Main.f90'), 'r') as file:
     for line in file:
-        if re.search("RCONST\s*=\s*RCONST\s*\\*\s*R_F",line.strip('\n').upper()):
+        if re.search(r"RCONST\s*=\s*RCONST\s*\*\s*R_F",line.strip('\n').upper()):
             hits +=1
-        elif re.search("REAL\(DP\),\s*SAVE\s*::\s*R_F\(NREACT\)",line.strip('\n').upper()):
+        elif re.search(r"REAL\(DP\),\s*SAVE\s*::\s*R_F\(NREACT\)",line.strip('\n').upper()):
             hits +=1
-        elif re.search("\s*!\s*MODIFY\s*RATE\s*CONSTANTS",line.strip('\n').upper()):
+        elif re.search(r"\s*!\s*MODIFY\s*RATE\s*CONSTANTS",line.strip('\n').upper()):
             hits +=1
-        elif re.search('SUBROUTINE KPP_SETUP\(\)', line.strip('\n').upper()) != None:
+        elif re.search(r'SUBROUTINE KPP_SETUP\(\)', line.strip('\n').upper()) != None:
             mainfile += '  SUBROUTINE KPP_SetUp(R_F_in)\n  REAL(DP), OPTIONAL :: R_F_in(NREACT)\n  IF (PRESENT(R_F_in)) R_F = R_F_in\n'
             hits +=1
 
@@ -56,10 +56,10 @@ mainfile = ''
 search_on = True
 with open(os.path.join(f,'second_Global.f90'), 'r') as file:
     for line in file:
-        if re.search("REAL\(DP\),\s*SAVE\s*::\s*R_F\(NREACT\)\s*=\s*1D0",line.strip('\n').upper()):
+        if re.search(r"REAL\(DP\),\s*SAVE\s*::\s*R_F\(NREACT\)\s*=\s*1D0",line.strip('\n').upper()):
             hits +=1
 
-        if re.search('!\s*INLINED\s*GLOBAL\s*VARIABLE\s*DECLARATIONS', line.upper()) and hits == 0 and search_on:
+        if re.search(r'!\s*INLINED\s*GLOBAL\s*VARIABLE\s*DECLARATIONS', line.upper()) and hits == 0 and search_on:
             mainfile += '  ! Enables changing reactions rates from the outside of chemistry\n'
             mainfile += '  REAL(DP), SAVE :: R_F(NREACT) = 1D0\n'
             search_on = False
