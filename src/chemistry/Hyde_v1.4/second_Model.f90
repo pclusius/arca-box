19e941bf297887bfe95e70abc67a8638408b3044
pclusius <petri.clusius@helsinki.fi>
2025-12-03 22:45:22 +0200

    chemistry for v1.4. updated NAMES

---
+++ src/chemistry/Hyde_v1.4/second_Model.f90
@@ -0,0 +1,20 @@
+MODULE second_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model second
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE second_Precision
+  USE second_Parameters
+  USE second_Global
+  USE second_Function
+  USE second_Integrator
+  USE second_Rates
+  USE second_Jacobian
+  USE second_LinearAlgebra
+  USE second_Monitor
+  USE second_Util
+
+END MODULE second_Model
+
---
+++ src/chemistry/Hyde_v1.4/second_reactivity.f90
@@ -0,0 +1,28 @@
+module second_reactivity
+
+  implicit none
+
+  private
+
+  integer, parameter :: NREACTIVITY=0
+
+  character(len=20), parameter :: reactivity_name(NREACTIVITY) = (/ &
+character(len=20):: &
+ /)
+
+  public :: calculate_reactivities, NREACTIVITY, reactivity_name
+
+contains
+
+  subroutine calculate_reactivities(CONC, reactivity)
+
+    use second_Precision   ! dp
+    use second_Parameters  ! NSPEC, ind_*
+    use second_Global      ! TEMP, M, O2, N2, H2O, RO2, K values
+
+    real(dp), intent(in) :: CONC(NSPEC)
+    real(dp), intent(out) :: reactivity(NREACTIVITY)
+
+  end subroutine calculate_reactivities
+
+end module second_reactivity
